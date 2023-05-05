3b0554817fc21a2923a79bc16c5983e3ec7d5b7d
Hedgedog <hedgedog@localhost.localdomain>
2023-05-05 11:35:16 +0200

    Improving at Raycasting

--- includes/structs.h
+++ includes/structs.h
@@ -6,7 +6,7 @@
 /*   By: jaizpuru <jaizpuru@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 15:48:57 by jaizpuru          #+#    #+#             */
-/*   Updated: 2023/05/04 15:53:18 by jaizpuru         ###   ########.fr       */
+/*   Updated: 2023/05/05 10:09:43 by jaizpuru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,12 @@ typedef	struct s_vector
 
 typedef struct s_grid
 {
-	int	mapWeight; // 2d grid
+	char	**grid;
+
+	int	mapHeight; // 2d grid
 	int	mapWidth;
 
-	int	screenWeight; // minilibX-image
+	int	screenHeight; // minilibX-image
 	int	screenWidth;
 
 	int	posX; // starting pos of character & Pos Vector
@@ -36,6 +38,23 @@ typedef struct s_grid
 
 	int	planeX; // Camera Vector
 	int	planeY;
+	
+	// Unitary Double Variables
+
+	double	u_mapHeight; // 2d grid
+	double	u_mapWidth;
+
+	double	u_screenHeight; // minilibX-image
+	double	u_screenWidth;
+
+	double	u_posX; // starting pos of character & Pos Vector
+	double	u_posY;
+
+	double	u_dirX; // initial pos of Direction Vector
+	double	u_dirY;
+
+	double	u_planeX; // Camera Vector
+	double	u_planeY;
 } t_grid;
 
 typedef	struct s_mlx

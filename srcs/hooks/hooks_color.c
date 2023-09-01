e248a0cfd296e0d88bbe13ff9589cd3b4256b854
Jokin <jokinaizpuru28@gmail.com>
2023-09-01 10:40:37 +0200

    Check everything works fine, 0 leaks, all hooks right, Norminette works fine, Ready for evaluation

--- srcs/hooks/hooks_color.c
+++ srcs/hooks/hooks_color.c
@@ -6,7 +6,7 @@
 /*   By: jaizpuru <jaizpuru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 18:54:18 by jaizpuru          #+#    #+#             */
-/*   Updated: 2023/08/28 16:47:41 by jaizpuru         ###   ########.fr       */
+/*   Updated: 2023/09/01 10:31:14 by jaizpuru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	change_floor_color(t_in *in)
 {
-	in->grid->vec->c->floor_color = 0x00A8A495;
+	in->grid->vec->c->floor_color *= 2;
 }
 
 void	change_ceiling_color(t_in *in)
 {
-	in->grid->vec->c->ceiling_color = 0x6289FF00;
+	in->grid->vec->c->ceiling_color *= 2;
 }
